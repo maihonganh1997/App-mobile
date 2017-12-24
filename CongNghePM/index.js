044403650c6501702ed829b000ee570e6ea61f1b
anhmh <anhmh@bloomgoo.vn>
2017-12-25 01:52:11 +0700

    no message

--- CongNghePM/index.js
+++ CongNghePM/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 
 import Router from "./src/components/Router";
-import TaskFlatList from "./src/components/screen_2/TaskFlastList";
+
 
 AppRegistry.registerComponent('CongNghePM', () => Router);
